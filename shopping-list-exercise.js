b3b6adb5bc86bbb7d8f7f07c0e22969fe6d50dfb
hak <hak@tutao.de>
2024-10-14 11:04:19 +0200

    add value to the list, create deleteBtn and its function, style. Clean the input after rendering.

--- shopping-list-exercise.js
+++ shopping-list-exercise.js
@@ -3,9 +3,35 @@ const input = document.querySelector("#item");  //pull the input from html
 const ul = document.querySelector("#ul")        //pull the ul from html
 
 btn.addEventListener("click", function() { // Add the function: when I click on the button, I have this:
+    if(input.value.trim() === "") {
+        alert ("Please enter an item.");
+        return; 
+    }
 let list = document.createElement("li"); //Create a list element
-list.textContent = input.value; // Read the value from the input provided
-ul.appendChild(list);  // Add new list item to the list 
+const deleteBtn = document.createElement("button"); // create "delete" button
+const spanElem = document.createElement("span"); // create "span" 
+// Add text content  "Delete" to the button 
+deleteBtn.style.color = "black";
+
+
+spanElem.textContent = input.value;  // Span is the value from the input provided
+deleteBtn.textContent = "Delete";
+
+list.appendChild(spanElem); // Add "span"  to the list element
+ul.appendChild(list);  // Add new list element to the list 
+list.appendChild(deleteBtn); // Add "delete" button to the list element
+
 input.value = ""; // Clears the input field after adding the item
+
+deleteBtn.addEventListener("click", () => {
+    ul.removeChild(list);
 });
+
+input.focus();
+
+});
+
+
+
+
 
